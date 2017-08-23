276b1262fd2454fbc18a1d5d8adf3976c691fa4a
balbini <balbini91@gmail.com>
2017-08-23 12:00:17 -0700

    added animations

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,25 @@
 $(document).ready(function(){
   console.log("linked");
-let playerOne = 0;
-let playerTwo = 0;
+  let playerOne = 0;
+  let playerTwo = 0;
+  let finish = 20;
 
-$(document).ready(function beginRace(){
-  $("body").keypress(function(e){
-    if (e.charCode === 47){
-    console.log("keypress");
-  } else if (e.charCode === 122) {
-    console.log("right key");
-  } else {
-    console.log("incorrect");
-  }
-});
-});
-});
+  function beginRace(){
+    $(document).keypress(function(e){
+      if (e.charCode === 47){
+        playerOne ++;
+          $(".flash-run").animate({left:"+=50px"})
+          console.log("flash moved");
+          console.log(playerOne);
+        } else if (e.charCode === 122) {
+          playerTwo ++;
+            $(".reverse-flash-run").animate({left:"+=50px"})
+            console.log("reverse flash moved")
+            console.log(playerTwo)
+        } else {
+          alert("Wrong key!")
+        };
+      });
+    };
+    beginRace();
+  });

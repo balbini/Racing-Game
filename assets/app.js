4bcea7b4a25e68f0ed277d04df13003b734eedae
balbini <balbini91@gmail.com>
2017-08-23 10:57:46 -0700

    added keypress event listeners and images

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
   console.log("linked");
+let playerOne = 0;
+let playerTwo = 0;
+
+$(document).ready(function beginRace(){
+  $("body").keypress(function(e){
+    if (e.charCode === 47){
+    console.log("keypress");
+  } else if (e.charCode === 122) {
+    console.log("right key");
+  } else {
+    console.log("incorrect");
+  }
+});
+});
 });

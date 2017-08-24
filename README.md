dca1f095aa322dfbcb52105351dc471490d64911
balbini <balbini91@gmail.com>
2017-08-23 17:52:28 -0700

    readme finished, refresh button working

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Racing-Game
 My version of a racing game.
+
+# Technologies Used
+jQuery, HTML, CSS
+
+# Code I'm Proud Of
+playerOne ++;
+$(".flash-run").animate({left:"+=9.9%"}, 0100)
+if (playerOne > finish){
+  $("h1").text("Barry Allen is the fastest man alive!")
+
+I was able to get this animate function to work fairly early in this process. I am
+proud of it because it was one of the first times I was able to get a difficult function to work
+without asking for help. Though I did have to get clarification on how bring it to other areas
+correctly. Being able to do something using my own knowledge and skills felt awesome.
+
+# Screenshot
--- assets/app.js
+++ assets/app.js
@@ -8,28 +8,26 @@ $(document).ready(function(){
       // player one action
       if (e.charCode === 47){
         playerOne ++;
-        $(".flash-run").animate({left:"+=9.9%"}, 0100)
-        if (playerOne > finish){
-          $("h1").text("Barry Allen is the fastest man alive!")
-        }
+        $(".flash-run").animate({left:"+=8%"}, 0100)
+        // sanity check
         console.log(playerOne);
         console.log(finish);
         // player two action
       } else if (e.charCode === 122) {
         playerTwo ++;
         $(".reverse-flash-run").animate({left:"+=8%"},0100)
-        if (playerTwo > finish){
-          $("h1").text("Jay Garrick is the fastest man alive!")
-        }
+        // sanity check
         console.log(playerTwo)
       }
-
+      // Game shutoff
       if (playerOne === 10 || playerTwo === 10){
         $(document).off("keypress");
       };
+      // when player one wins
       if (playerOne === finish){
         $("h1").text("Barry Allen is the fastest man alive!")
       };
+      // when player two wins
       if (playerTwo === finish){
         $("h1").text("Jay Garrick is the fastest man alive!")
       };
